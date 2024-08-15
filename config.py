b0069e8235d81d9b4239f8eac5f073df629b46a8
vip-02 <omkarm01010@gmail.com>
2024-08-15 13:44:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ["API_ID", "18156248"]
+    API_HASH = os.environ["API_HASH", "db946fb6805b1a698c679626b617e77a"]
+    AUTH_USERS = ("2032347579")
+# jisko bhi is repo ko deploy karne me error aa raha ho ( https://t.me/Stubborn1223 ) ye mere TG ka id ha message karke iska screen shot send kar dena me error solve kar dunga. 
